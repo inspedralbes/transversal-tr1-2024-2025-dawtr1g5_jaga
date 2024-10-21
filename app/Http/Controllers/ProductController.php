f1d7dd37bc20984b13025352df87240d5905cfb5
a23jonbetcas <a23jonbetcas@inspedralbes.cat>
2024-10-21 13:22:17 +0200

    crear taula cart

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * index per mostrar tots els productes
+     * create per inserir un nou producte
+     * update per actulitzar producte
+     * destroy per eliminar un producte
+    */
+
+    //veure tots els productes
+    /*public function index(){
+        $todos = Todo::all(); //
+        return view('.index',);//nom del index
+    }*/
+
+    //Inserir un nou producte a la base de dades
+    /*public function store(Request $request): void{
+        $request->validate([
+            "title"=> "",
+            "description"=>"",
+            "price"=> "",
+            "stock"=> "",
+            "url"=> "",
+        ]);
+    }*/
+}

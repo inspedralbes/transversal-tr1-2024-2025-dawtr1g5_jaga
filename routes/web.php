6de2b8176c1570fa13b044ed5c5b68ee632a86a6
GerardArias <a23gerarimar@inspedralbes.cat>
2024-10-22 11:01:20 +0200

    restart project

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-//Ruta al crud
-Route::get('/back', function () {
-    return view('crud');
-});
-
-Route::get('/cart', function () {
-    
-    return view('cart');
-});

c7cf4060062dfcaf7e648012d501a029fb96c17a
JoselynNC1999 <a23josnincal@inspedralbes.cat>
2024-10-22 10:22:09 +0200

    web cambiada

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,8 @@ Route::get('/', function () {
 Route::get('/productes', function () {
     $json = File::get(storage_path('../database/data/products.json'));
 
-    // Decodificar el JSON en un array PHP
     $productes = json_decode($json, true);
 
-    // Pasar los productos a la vista
     return view('productes', ['productes' => $productes]);
 });
 

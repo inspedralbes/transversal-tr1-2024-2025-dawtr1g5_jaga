b2ea051810cfd8f7089258dd807399964ee75fa6
JoselynNC1999 <a23josnincal@inspedralbes.cat>
2024-10-22 10:10:40 +0200

    listaProductos

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/productes', function () {
+    $json = File::get(storage_path('../database/data/products.json'));
+
+    // Decodificar el JSON en un array PHP
+    $productes = json_decode($json, true);
+
+    // Pasar los productos a la vista
+    return view('productes', ['productes' => $productes]);
+});
+
+
+
+

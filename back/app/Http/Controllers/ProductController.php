d2d8b30c73c23f4f5a5afba9a7f26bb38d67c9c1
a23agunovnov <a23agunovnov@inspedralbes.cat>
2024-10-22 15:28:43 +0200

    API con index, show, update y destroy acabada

--- back/app/Http/Controllers/ProductController.php
+++ back/app/Http/Controllers/ProductController.php
@@ -4,31 +4,123 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
     /**
-     * index per mostrar tots els productes
-     * create per inserir un nou producte
-     * update per actulitzar producte
-     * destroy per eliminar un producte
-    */
-
-    //veure tots els productes
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $products = Product::all(); 
-        return view('index', compact('products')); 
+        $products = Product::all();
+        return response()->json($products);
     }
 
-    //Inserir un nou producte a la base de dades
-    /*public function store(Request $request): void{
-        $request->validate([
-            "title"=> "",
-            "description"=>"",
-            "price"=> "",
-            "stock"=> "",
-            "url"=> "",
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "title" => "required",
+            "description" => "required",
+            "price" => "required",
+            "stock" => "required"
         ]);
-    }*/
+
+        if ($validator -> fails()){
+            return response()->json(["message"=>"Error en la validación de los datos"]);
+        }else{
+            $product = Product::create([
+                'title' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+                'stock' => $request->stock,
+            ]);
+
+            if(!$product){
+                return response()->json([
+                    "message" => "Error al crear el producto",
+                    "status" => 404
+                ]);
+            }
+
+            return response()->json([
+                "product" => $product,
+                "status" => 200
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        if(!empty($product)){
+            return response()->json([
+                "product" => $product,
+                "status" => 200
+            ]);
+        }else{
+            return response()->json([
+                "message" => "El producto no se encuentra",
+                "status" => 404
+            ]);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        if(!$product){
+            return response() -> json([
+                "message" => "El producto no existe",
+                "status" => 404
+            ]);
+        }
+        $validator = Validator::make($request->all(),[
+            "title" => "required",
+            "description" => "required",
+            "price" => "required",
+            "stock" => "required"
+        ]);
+
+        if($validator->fails()){
+            return response() -> json([
+                "message" => "Los datos no son válidos",
+                "status" => 404
+            ]);
+        }
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->save();
+
+        return response()->json([
+            "product" => $product,
+            "status" => 200
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+
+        return response()->json([
+            "message" => "Registro eliminado correctamente",
+            "status" => 200
+        ]);
+    }
 }

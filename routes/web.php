0441792e8bbee8020a7afae997abf2ced14c1a83
GerardArias <a23gerarimar@inspedralbes.cat>
2024-10-22 11:02:05 +0200

    a

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Ruta al crud
-Route::get('/back', function () {
-    return view('crud');
-});
-
-Route::get('/cart', function () {
-    
-    return view('cart');
-});

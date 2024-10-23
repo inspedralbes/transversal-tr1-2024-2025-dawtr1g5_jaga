db818283f0456bb61ae18f05099b88b5139f3f87
Aivan Antonio <a21aivantjeh@inspedralbes.cat>
2024-10-23 13:04:00 +0200

    pagina cart

--- front/js/script.js
+++ front/js/script.js
@@ -1,51 +1,28 @@
-import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import { createApp, reactive, onBeforeMount } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import { getProducts } from './communicationManager.js';
 
 createApp({
     setup() {
-        const productes = ref([]); 
+        const productes =  reactive({datos:[]})
+        let preuTotal  = reactive({ total: 0 });
+        
+        onBeforeMount(async () => {
+            const data = await getProducts();
+            console.log(data);
+            
+            productes.datos = data;
 
-        fetch(`http://127.0.0.1:8000/api/products`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(info => {
-                productes.value = info; 
-                console.log(productes.value);
-            })
-            .catch(error => {
-                console.error('Error loading products:', error);
-            });
+            // Calcular el preu total de tots els productes dins del carret
+            for (let index = 0; index < productes.datos.length; index++) {
+                preuTotal.total += productes.datos[index].price;
+                //console.log(productes.datos[index].price);
+                console.log(preuTotal.total)
+            }
+        })
+
 
         return {
-            productes 
+            productes, preuTotal
         };
     }
 }).mount('#appVue');
-
-/*function mostrarCarret(productes) {
-    let htmlString = `<div class="cartHead">
-                        <h1>CART</h1>
-                      </div>
-                      <div class="cartBody">`;  
-    productes.sort(() => Math.random() - 0.5);
-    for (let index = 0; index < productes.length; index++) {
-        let product = productes[index];
-        htmlString += `<div class="productItem">
-                           <img src="${product.image}">
-                           <h4>${product.title}</h4>
-                           <div class="butons">
-                               <input type="button" value="-">
-                               <input type="text" id="macarrons" name="id1" min=0 max=9>
-                               <input type="button" value="+">
-                           </div>
-                           <p class="productPrice">€${product.price.toFixed(2)}</p>
-                           <p class="remove">&#10060;</p>
-                       </div>`;
-    }
-    htmlString += `</div>`;
-    document.getElementById('cartContainer').innerHTML = htmlString;
-}*/
-

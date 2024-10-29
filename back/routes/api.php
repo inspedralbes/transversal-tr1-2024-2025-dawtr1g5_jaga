9a6f9818ca96a0cc02f88ede40bc8c4979975c8e
a23agunovnov <a23agunovnov@inspedralbes.cat>
2024-10-29 19:35:52 +0100

    StripeIntegration pausada para mas adelante

--- back/routes/api.php
+++ back/routes/api.php
@@ -17,3 +17,17 @@ Route::delete('/products/{id}', [ProductController::class,'destroy']);
 
 Route::get('/orders', [CartController::class, 'index']);
 Route::post('/createOrder', [CartController::class, 'create']);
+ 
+// Route::get('/checkout', function (Request $request) {
+//     $stripePriceId = 'price_deluxe_album';
+ 
+//     $quantity = 1;
+ 
+//     return $request->user()->checkout([$stripePriceId => $quantity], [
+//         'success_url' => route('checkout-success'),
+//         'cancel_url' => route('checkout-cancel'),
+//     ]);
+// })->name('checkout');
+ 
+// Route::view('/checkout/success', 'checkout.success')->name('checkout-success');
+// Route::view('/checkout/cancel', 'checkout.cancel')->name('checkout-cancel');

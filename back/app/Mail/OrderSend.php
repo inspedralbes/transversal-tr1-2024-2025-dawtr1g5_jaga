68a6d97fd609a62542e68c719f454863009254e1
JoselynNC1999 <a23josnincal@inspedralbes.cat>
2024-11-06 23:19:48 +0100

    ticket de la comanda fet

--- back/app/Mail/OrderSend.php
+++ back/app/Mail/OrderSend.php
@@ -8,33 +8,30 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use App\Models\orderfinal;
+use App\Models\OrderFinal;  
 
 class OrderSend extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct(public orderfinal $totalOrder)
-    {
+    public OrderFinal $totalOrder;
+    public array $orderedProducts;
 
+    
+    public function __construct(OrderFinal $totalOrder, array $orderedProducts)
+    {
+        $this->totalOrder = $totalOrder;
+        $this->orderedProducts = $orderedProducts;
     }
 
-    /**
-     * Get the message envelope.
-     */
+    
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Order Sent',
+            subject: 'Order Confirmation',
         );
     }
 
-    /**
-     * Get the message content definition.
-     */
     public function content(): Content
     {
         return new Content(
